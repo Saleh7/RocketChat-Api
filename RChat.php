2ed09e7ba547678210f2e6d8d24426da0e12f50f
~/Saleh <iPain7@gmail.com>
2016-02-16 01:08:35 +0300

    Update RChat.php

--- RChat.php
+++ RChat.php
@@ -6,80 +6,76 @@
 require 'Curl.php';
 
 class RocketChat {
-    private $AuthToken = "";    // Authentication Token
-    private $UserId = "";       // User ID
-    private $UrlApi = "";       // URL to Rocket Chat APIs
 
     function __construct(){
         $this->Curl = new Curl();
     }
 
     //Obtaining the running Rocket.Chat version via REST API
-    public function RC_version() {
-        $version = $this->Curl->get($this->UrlApi.'version');
+    public function version() {
+        $version = $this->Curl->get($this->urlApi.'version');
         return $version;
     }
 
     // Login with REST API
-    public function RC_login($username, $password, $rc_url) {
-        $data['user'] = $username;
+    public function login($username, $password , $rc_url) {
+        $data['user']     = $username;
         $data['password'] = $password;
-        $this->UrlApi = $rc_url . "/api/";   // Save the URL for the other functions
-        $login = $this->Curl->post($this->UrlApi.'login', $data);
+        $this->urlApi = $rc_url . "/api/";   // Save the URL for the other functions
+        $login = $this->Curl->post($this->urlApi.'login', $data);
         if ($login->status == "success") {
-            $this->AuthToken = $login->data->authToken;
-            $this->UserId = $login->data->userId;
+            $this->authToken = $login->data->authToken;
+            $this->userId = $login->data->userId;
         }
         return $login;
     }
 
     // Logout with REST API
-    public function RC_logout() {
-        $this->Curl->setHeader('X-Auth-Token', $this->AuthToken);
-        $this->Curl->setHeader('X-User-Id', $this->UserId);
-        $logout = $this->Curl->get($this->UrlApi.'logout');
+    public function logout() {
+        $this->Curl->setHeader('X-Auth-Token', $this->authToken);
+        $this->Curl->setHeader('X-User-Id', $this->userId);
+        $logout = $this->Curl->get($this->urlApi.'logout');
         return $logout;
     }
 
     // Get list of public rooms via REST API
-    public function RC_publicRooms() {
-        $this->Curl->setHeader('X-Auth-Token', $this->AuthToken);
-        $this->Curl->setHeader('X-User-Id', $this->UserId);
-        $listRooms = $this->Curl->get($this->UrlApi.'publicRooms');
+    public function publicRooms() {
+        $this->Curl->setHeader('X-Auth-Token', $this->authToken);
+        $this->Curl->setHeader('X-User-Id', $this->userId);
+        $listRooms = $this->Curl->get($this->urlApi.'publicRooms');
         //return list public rooms
         return $listRooms;
     }
 
     // Join a room via REST API
-    public function RC_join($ID_Rooms) {
-        $this->Curl->setHeader('X-Auth-Token', $this->AuthToken);
-        $this->Curl->setHeader('X-User-Id', $this->UserId);
-        $join = $this->Curl->post($this->UrlApi."rooms/$ID_Rooms/join");
+    public function join($idRooms) {
+        $this->Curl->setHeader('X-Auth-Token', $this->authToken);
+        $this->Curl->setHeader('X-User-Id', $this->userId);
+        $join = $this->Curl->post($this->urlApi."rooms/$idRooms/join");
         return $join;
     }
 
     // Leave a room via REST API
-    public function RC_leave($ID_Rooms) {
-        $this->Curl->setHeader('X-Auth-Token', $this->AuthToken);
-        $this->Curl->setHeader('X-User-Id', $this->UserId);
-        $leave = $this->Curl->post($this->UrlApi."rooms/$ID_Rooms/leave");
+    public function leave($idRooms) {
+        $this->Curl->setHeader('X-Auth-Token', $this->authToken);
+        $this->Curl->setHeader('X-User-Id', $this->userId);
+        $leave = $this->Curl->post($this->urlApi."rooms/$idRooms/leave");
         return $leave;
     }
 
     // Get all unread messages in a room via REST API
-    public function RC_messages($ID_Rooms) {
-        $this->Curl->setHeader('X-Auth-Token', $this->AuthToken);
-        $this->Curl->setHeader('X-User-Id', $this->UserId);
-        $messages = $this->Curl->get($this->UrlApi."rooms/$ID_Rooms/messages");
+    public function getMessages($idRooms) {
+        $this->Curl->setHeader('X-Auth-Token', $this->authToken);
+        $this->Curl->setHeader('X-User-Id', $this->userId);
+        $messages = $this->Curl->get($this->urlApi."rooms/$idRooms/messages");
         //return all messages
         return $messages;
     }
-
     // Sending a message via REST API
-    public function RC_send($ID_Rooms,$msg) {
-        $this->Curl->setHeader('X-Auth-Token', $this->AuthToken);
-        $this->Curl->setHeader('X-User-Id', $this->UserId);
-        $send = $this->Curl->post($this->UrlApi."rooms/$ID_Rooms/send",array('msg' => $msg));
+    public function sendMessage($idRooms,$message) {
+        $this->Curl->setHeader('X-Auth-Token', $this->authToken);
+        $this->Curl->setHeader('X-User-Id', $this->userId);
+        $send = $this->Curl->post($this->urlApi."rooms/$idRooms/send",array('msg' => $message));
         return $send;
     }
 
